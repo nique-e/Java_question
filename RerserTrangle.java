990f6cf6df6eeff9215ee3012ca9880b2574f270
nique-e <niteshmule023@gmail.com>
2025-10-18 15:17:33 +0530

    Rerser Trangle start Pattern print

---
+++ RerserTrangle.java
@@ -0,0 +1,17 @@
+public class RerserTrangle{
+
+    public static void main(String[] args) {
+        
+        for(int i =1; i<=5; i++){
+            for(int s = 1; s<=i; s++){
+              
+                System.out.print(" ");
+            }
+            for(int j = 5; j>=i; j--){
+                System.out.print("*");
+            }
+
+            System.out.println("");
+        }
+    }
+}
