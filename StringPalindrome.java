f7ee1f376a65cb3bb6d10836bd5f6f259d4a777c
nique-e <niteshmule023@gmail.com>
2025-10-20 23:09:01 +0530

    Liner search opertion has been sol

--- StringPalindrome.java
+++ StringPalindrome.java
@@ -1,5 +1,14 @@
  public class StringPalindrome{
     public static void main(String[] args) {
+
+        String str = "madam";
+        String rev = "";
+
+        for(int i = str.length()-1; i>=0; i--){
+            rev = rev + str.charAt(i);
+        }
+
+        System.out.println(rev);
         
     }
  }
