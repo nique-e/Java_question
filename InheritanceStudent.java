d4df5aa8ed3903b2c72dfa7616270734ab416f89
nique-e <niteshmule023@gmail.com>
2025-10-21 00:16:45 +0530

    class are the craeted

--- InheritanceStudent.java
+++ InheritanceStudent.java
@@ -1,5 +1,18 @@
- public class InheritanceStudent{
 
+ class student{
+    String name;
+    int rollNumber;
+    float marks;
+
+    void display(){
+        System.out.println("Name :" +name);
+        System.out.println("Rollnumber :" +rollNumber);
+        System.out.println("Marks" +marks);
+    }
+
+ }
+ 
+ public class InheritanceStudent{
     public static void main(String[] args) {
         
     }
