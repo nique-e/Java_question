917517ea4ad98b071e6e192172fb75d41e549377
nique-e <niteshmule023@gmail.com>
2025-10-21 15:30:26 +0530

    that are class cratead sol problem

--- Bank.java
+++ Bank.java
@@ -1,6 +1,32 @@
 class bankAccount{
 
+    private double balance;
 
+    public bankAccount(double  initialBalance){
+        balance = initialBalance;
+    }
+
+    public void deposit(double amount){
+        balance = balance + amount;
+        System.out.println(amount+ "desposit. current Balance :" +balance);
+
+    }
+
+    public void withdraw(double amount){
+        if(amount <= balance){
+            balance = balance -amount;
+            System.out.println(amount+ "withdraw.remaining balance :" +balance);
+
+        }
+        else{
+            System.out.println("Insufficient balance! withdraw failed.");
+        }
+    }
+
+    public void displayBalance(){
+
+        System.out.println("Current Balance:" +balance);
+    }
 }
 
 public class Bank{
