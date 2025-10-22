cb2f3e01c87eb1bb9280ad34c790608491c5b096
nique-e <niteshmule023@gmail.com>
2025-10-22 23:20:58 +0530

    number is ha been print

--- Number.java
+++ Number.java
@@ -2,6 +2,11 @@ public class Number{
 
     public static void main(String[] args) {
         
-        
+        for(int i = 1; i<=4; i++){
+            for(int j = i; j>=1; j--){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
     }
 }
