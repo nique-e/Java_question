a56e469eece876fe55d0ed4e07b67d8c80f1d542
nique-e <niteshmule023@gmail.com>
2025-10-24 22:54:22 +0530

    for loop are is createrd

--- PrintArray.java
+++ PrintArray.java
@@ -3,6 +3,11 @@ public class PrintArray{
     public static void main(String[] args) {
         String names[] = {"Nitersh", "Rushi", "Sahil", "Kunal", "Shailesh"};
         System.out.print("index |" +"names");
+        System.out.println("");
+
+        for(int i = 0; i<names.length; i++){
+            System.out.println(i+ "  |" +names[i]);
+        }
        
     }
 }
