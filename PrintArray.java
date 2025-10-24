ee87fb8527eae18dd85c1c4ae7c087e3be5d2c65
nique-e <niteshmule023@gmail.com>
2025-10-24 22:52:05 +0530

    array are is decleare

--- PrintArray.java
+++ PrintArray.java
@@ -1,6 +1,8 @@
 public class PrintArray{
 
     public static void main(String[] args) {
+        String names[] = {"Nitersh", "Rushi", "Sahil", "Kunal", "Shailesh"};
+        System.out.print("index |" +"names");
        
     }
 }
